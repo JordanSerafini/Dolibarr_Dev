c1a0548efe3305bd98e08780be402891268999e5
JordanS <jordanserafini.74@gmail.com>
2025-09-29 16:33:59 +0200

    work

---
+++ Data_Driven/src/__init__.py
@@ -0,0 +1,7 @@
+"""
+🚀 DOLIBARR DATA-DRIVEN SYSTEM
+Système intelligent d'analyse des données BTP avec DataLake, Warehouse et Smart Analytics
+"""
+
+__version__ = "1.0.0"
+__author__ = "Dolibarr BTP Team"
